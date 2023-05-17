87400f9a4069538d4636def7d9d276467b8537cb
Teuthida <33849310+Teuthida255@users.noreply.github.com>
2023-05-16 21:18:44 -0700

    Ice and player edits
    >Made Ice a 2D vector of smart pointers
    >Created a single Actor class for all game objects (this is a requirement for part 1)
    >Resolved the circular dependency that prevented IceMan from using a StudentWorld reference

--- Actor.h
+++ Actor.h
@@ -3,60 +3,79 @@
 
 #include "GraphObject.h"
 #include <vector>
+#include <ranges> // requires C++20
 #include "GameWorld.h"
 
 const int ACTOR_WIDTH_LIMIT = VIEW_WIDTH - 4;
 const int ACTOR_HEIGHT_LIMIT = VIEW_HEIGHT - 4;
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
-class IceMan : public GraphObject {
+class StudentWorld; // Gordian knotting the circular dependency
+
+class Actor : public GraphObject {
 public:
-	IceMan(GameWorld& world, int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
-		: GraphObject(imageID, startX, startY, dir, size, depth), w(world)  {
+	Actor(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+	: GraphObject(imageID, startX, startY, dir, size, depth) {
+		setVisible(true);
+	}
+
+	virtual void doSomething() { }
+};
+
+// further class modualarization to come
+class IceMan : public Actor {
+public:
+	IceMan(StudentWorld& world, int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+		: Actor(imageID, startX, startY, dir, size, depth), w(world)  {
 		setVisible(true);
 	}
 	void doSomething();
-	~IceMan() { }
 
-	GameWorld& w; // passed in during init()
+	StudentWorld& w; // passed in during init()
 };
 
-class IceBlock : public GraphObject {
+class IceBlock : public Actor {
 public:
 	IceBlock(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
-		: GraphObject(imageID, startX, startY, dir, size, depth) {
+		: Actor(imageID, startX, startY, dir, size, depth) {
 		setVisible(true);
 	}
-	~IceBlock() {
-		delete this;
-	}
 };
+
+const int TUNNEL_START_X = 30;
+const int TUNNEL_END_X = 33;
+const int TUNNEL_END_Y = 4;
+const int ACTOR_HEIGHT = 4;
 
 class Ice {
 public:
-	Ice() { //create an array of ice objects
-		for (int i{}; i < VIEW_WIDTH; i++) { //cover the width
-			for (int j{}; j < VIEW_HEIGHT; j++) { //cover the height
-				if (i < ((VIEW_WIDTH / 2) - 1) || i >((VIEW_WIDTH / 2) + 1) || (j < VIEW_HEIGHT / 2)) { //create a tunnel
-					iceObjects[i, j] = new IceBlock(IID_ICE, i, j, GraphObject::right, .25, 3);
+	Ice() {
+		// from <ranges>
+		// think python ranges, but C++ified (i.e. painfully verbose)
+		for (auto i : std::ranges::iota_view(0, VIEW_WIDTH)) {
+			for (auto j : std::ranges::iota_view(0, VIEW_HEIGHT - ACTOR_HEIGHT)) {
+				if ((i < TUNNEL_START_X) || (i > TUNNEL_END_X) || (j <= TUNNEL_END_Y)) {
+					iceObjects[i][j] = std::make_unique<IceBlock>(IID_ICE, i, j, GraphObject::right, .25, 3);
 				}
 			}
 		}
 	}
-	~Ice() {
-		for (int i{}; i < VIEW_WIDTH; i++) {
-			for (int j{}; j < VIEW_HEIGHT; j++) {
-				delete iceObjects[i, j]; //delete 2d array
-			}
-		}
+	// smart pointers mean that we don't have to delete anything ourselves
+	
+	std::shared_ptr<IceBlock>& getBlock(int x, int y) {
+		// includes bounds checking
+		return iceObjects.at(x).at(y);
 	}
 	
-	
 private:
-	IceBlock* iceObjects[];
+	// a less disgusting way to write this would be appreciated
+	// using namespace std would just be a stopgap
+	// making the elements unique_ptrs without incurring the compiler's wrath would also be cool
+	std::vector<std::vector<std::shared_ptr<IceBlock>>> iceObjects = std::vector<std::vector<std::shared_ptr<IceBlock>>>(VIEW_WIDTH, std::vector<std::shared_ptr<IceBlock>>(VIEW_HEIGHT, nullptr));
 
 };
 
 
 #endif // ACTOR_H_
+
 
