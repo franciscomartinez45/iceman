bbfa6787fd4b34ab82870296dd523e2f00d5ac88
Teuthida <33849310+Teuthida255@users.noreply.github.com>
2023-05-25 09:29:27 -0700

    Add Prop/Actor array to game

--- StudentWorld.h
+++ StudentWorld.h
@@ -8,19 +8,24 @@
 #include <memory>
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
 class StudentWorld : public GameWorld
 {
 public:
 	StudentWorld(std::string assetDir)
 		: GameWorld(assetDir)
 	{
+		srand(time(NULL));
 	}
 
 	virtual int init();
 
 	virtual int move();
 
-	virtual void cleanUp(){}
+	virtual void cleanUp(){
+		actors.clear();
+		goodies.clear();
+	}
 	
 	StudentWorld& getWorld() {
 		return *this;
@@ -36,6 +41,8 @@ private:
 	void setStatusBar();
 
 	std::unique_ptr<IceMan> player = nullptr;
+	std::vector<std::unique_ptr<Actor>> actors;
+	std::vector<std::unique_ptr<Prop>> goodies;
 	std::unique_ptr<Ice> ice = nullptr;
 };
 
