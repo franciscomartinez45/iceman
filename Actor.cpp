531131fa8df140ccc78423447231738bf710ba1d
Teuthida <33849310+Teuthida255@users.noreply.github.com>
2023-05-27 09:46:34 -0700

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -8,6 +8,10 @@ float Object::getDistanceTo(Object& other) {
 	return sqrt(pow(this->getX() - other.getX(), 2) + pow(this->getY() - other.getY(), 2));
 }
 
+float Object::getDistanceTo(std::pair<unsigned int, unsigned int> p) {
+	return sqrt(pow(this->getX() - p.first, 2) + pow(this->getY() - p.second, 2));
+}
+
 bool Object::contains(unsigned int x, unsigned int y) {
 	return ((x >= getX()) and (x < getX() + int(getSize() * 4)) and (y >= getY()) and (y < getY() + int(getSize() * 4)));
 }
@@ -17,87 +21,100 @@ bool Object::intersects(unsigned int x, unsigned int y, unsigned int x_size, uns
 		or contains(x, y + y_size - 1) or contains(x + x_size - 1, y));
 }
 
-// health is almost never set in the game,
-// so health can only be incremented or decremented
-void IceMan::offsetHealth(int offset) {
-	health += offset;
-	if (health > max_health)
-		health = max_health;
-	else if (health < 0)
-		health = 0;
-}
-
 void IceMan::doSomething() {
-	// remove intersecting ice
-	
-	if (w.getIce()->destroyIce(getX(), getY(), ACTOR_HEIGHT, ACTOR_HEIGHT))
-		w.playSound(SOUND_DIG);
-	int key;
-	if (IceMan::w.getKey(key)) {
-		Direction new_dir = none;
-		switch (key) {
-		case KEY_PRESS_UP:
-			new_dir = up;
-			break;
-		case KEY_PRESS_DOWN:
-			new_dir = down;
-			break;
-		case KEY_PRESS_LEFT:
-			new_dir = left;
-			break;
-		case KEY_PRESS_RIGHT:
-			new_dir = right;
-			break;
-		case KEY_PRESS_SPACE:
-			// TODO
-			break;
-		case KEY_PRESS_TAB:
-			// SEE ABOVE
-			break;
-		case KEY_PRESS_ESCAPE:
-			// CONTINUE SEEING ABOVE
-			break;
-		}
-		
-		if (new_dir != none) {
-			if (new_dir != getDirection())
-				setDirection(new_dir);
+	if (!isDead()) {
+		// remove intersecting ice
 
-			else {
-				auto new_pos = std::make_pair<int>(getX(), getY());
-				auto prev_pos = new_pos;
+		if (w.getIce()->destroyIce(getX(), getY(), ACTOR_HEIGHT, ACTOR_HEIGHT))
+			w.playSound(SOUND_DIG);
+		int key;
+		if (IceMan::w.getKey(key)) {
+			Direction new_dir = none;
+			switch (key) {
+			case KEY_PRESS_UP:
+				new_dir = up;
+				break;
+			case KEY_PRESS_DOWN:
+				new_dir = down;
+				break;
+			case KEY_PRESS_LEFT:
+				new_dir = left;
+				break;
+			case KEY_PRESS_RIGHT:
+				new_dir = right;
+				break;
+			case KEY_PRESS_SPACE:
+				// TODO
+				break;
+			case KEY_PRESS_TAB:
+				// SEE ABOVE
+				break;
+			case KEY_PRESS_ESCAPE:
+				beAnnoyed(INSTAKILL_DAMAGE);
+				break;
+			}
 
-				switch (new_dir) {
-				case up:
-					new_pos.second++;
-					break;
-				case down:
-					new_pos.second--;
-					break;
-				case left:
-					new_pos.first--;
-					break;
-				case right:
-					new_pos.first++;
-					break;
+			if (new_dir != none) {
+				if (new_dir != getDirection())
+					setDirection(new_dir);
+
+				else {
+					auto new_pos = std::make_pair<int>(getX(), getY());
+					auto prev_pos = new_pos;
+
+					switch (new_dir) {
+					case up:
+						new_pos.second++;
+						break;
+					case down:
+						new_pos.second--;
+						break;
+					case left:
+						new_pos.first--;
+						break;
+					case right:
+						new_pos.first++;
+						break;
+					}
+
+					// bounds checking
+					if (willCollide(new_pos))
+						new_pos = prev_pos;
+					moveTo(new_pos.first, new_pos.second);
 				}
-
-				// bounds checking
-				if (new_pos.first < 0 or new_pos.first > ACTOR_WIDTH_LIMIT or new_pos.second < 0 or new_pos.second > ACTOR_WIDTH_LIMIT)
-					new_pos = prev_pos;
-				moveTo(new_pos.first, new_pos.second);
 			}
 		}
 	}
 } // glorious bracket staircase
 
-const double WATER_PICKUP_DISTANCE = 3.0;
+void IceMan::beAnnoyed(int annoy_value) {
+	health -= annoy_value;
+	if (health <= 0) {
+		health = 0;
+		dead = true;
+		w.playSound(SOUND_PLAYER_GIVE_UP);
+	}
+}
+
+bool IceMan::willCollide(std::pair<int, int> new_pos) {
+	if (new_pos.first < 0 or new_pos.first > ACTOR_WIDTH_LIMIT or new_pos.second < 0 or new_pos.second > ACTOR_WIDTH_LIMIT)
+		return true;
+	
+	for (auto& goodie : w.getGoodies()) {
+		if (goodie->isBoulder() and goodie->intersects(new_pos.first, new_pos.second))
+			return true;
+	}
+
+	return false;
+}
+
+const double ITEM_PICKUP_DISTANCE = 3.0;
 const unsigned int WATER_PICKUP_AMOUNT = 5;
 const unsigned int WATER_PICKUP_SCORE = 100;
 
 void Water::doSomething() {
 	if (!dead) {
-		if (getDistanceTo(*(w.getPlayer().get())) <= WATER_PICKUP_DISTANCE) {
+		if (getDistanceTo(*(w.getPlayer().get())) <= ITEM_PICKUP_DISTANCE) {
 			dead = true;
 
 			w.playSound(SOUND_GOT_GOODIE);
@@ -111,6 +128,66 @@ void Water::doSomething() {
 				lifespan--;
 		}
 	}
+}
+
+void Boulder::doSomething() {
+	if (!isDead()) {
+		switch (state) {
+		case BoulderState::Stable:
+			if (!hasIceUnder())
+				state = BoulderState::Jostling;
+			break;
+
+		case BoulderState::Jostling:
+			if (jostleTimer == 0) {
+				state = BoulderState::Falling;
+				w.playSound(SOUND_FALLING_ROCK);
+			}
+			else
+				jostleTimer--;
+			break;
+
+		case BoulderState::Falling:
+			if (getY() == 0 or hasIceUnder() or hasBoulderUnder())
+				dead = true;
+			else {
+				moveTo(getX(), getY() - 1);
+				if (getDistanceTo(*(w.getPlayer().get())) <= ITEM_PICKUP_DISTANCE) {
+					w.getPlayer()->beAnnoyed(INSTAKILL_DAMAGE);
+				}
+				// TODO: ADD PROTESTER DAMAGE
+			}
+				
+			break;
+		}
+	}
+}
+
+bool Boulder::hasIceUnder() {
+	auto j = getY() - 1;
+	if (j < 0)
+		return false;
+
+	for (auto i : std::ranges::iota_view(getX(), getX() + ACTOR_HEIGHT)) {
+		if (w.getIce()->getBlock(i, j) != nullptr)
+			return true;
+	}
+	return false;
+}
+
+bool Boulder::hasBoulderUnder() {
+	if ((getY() - ACTOR_HEIGHT) < 0)
+		return false;
+
+	for (auto& goodie : w.getGoodies()) {
+		if (goodie->isBoulder()) {
+			if ((goodie->getY() == getY() + ACTOR_HEIGHT - 1)
+				and (goodie->getX() > getX() - ACTOR_HEIGHT)
+				and (goodie->getX() < getX() + ACTOR_HEIGHT))
+				return true;
+		}
+	}
+	return false;
 }
 
 Ice::Ice(StudentWorld& world) {
