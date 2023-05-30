8741d1407bd311b7a7ce57d54568f1eb83e76797
Teuthida <33849310+Teuthida255@users.noreply.github.com>
2023-05-30 11:47:59 -0700

    Practicing what I preach

--- StudentWorld.h
+++ StudentWorld.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <memory>
 #include <list>
+#include <random>
 
 enum class ObjectType {
 	Boulder,
@@ -69,6 +70,8 @@ private:
 	std::unique_ptr<IceMan> player = nullptr;
 	std::list<std::unique_ptr<Object>> objects;
 	std::unique_ptr<Ice> ice = nullptr;
+
+	std::default_random_engine generator;
 };
 
 #endif // STUDENTWORLD_H_
