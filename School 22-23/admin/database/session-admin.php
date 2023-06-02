8df116a885266065356aae3b4e4dcb9d52c80694
Francisco Martinez <105575991+franciscomartinez45@users.noreply.github.com>
2023-06-01 19:27:37 -0700

    Delete School 22-23 directory

--- School 22-23/admin/database/session-admin.php
+++
@@ -1,9 +0,0 @@
-<?php $sql = "SELECT * FROM users WHERE user_type = 'admin';";
-$result = mysqli_query($con,$sql);
-$resultCheck = mysqli_num_rows($result);
-	if ($resultCheck>0){
-	while($row = mysqli_fetch_assoc($result)){
-		$adminname=$row['name'];
-	}
-	}
-	?>
